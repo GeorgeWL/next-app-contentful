c62768f2480420c2deda399dbff77cca3ea417a6
GeorgeWL <georgelangham1990@gmail.com>
2020-06-05 13:46:01 +0100

    add eslint modules. add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+    'React': 'writeable'
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'settings': {
+    'react': {
+      'version': 'detect'
+    },
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'react/react-in-jsx-scope': 'off'
+  }
+}
