ff0f1e9add161d27c06f5be643d34a4851bbc75c
GeorgeWL <georgelangham1990@gmail.com>
2020-06-05 15:12:07 +0100

    add prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,59 @@
+/* eslint-disable quote-props */
 module.exports = {
   env: {
     browser: true,
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier/react',
+    'plugin:prettier/recommended',
+  ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    React: "writeable",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    React: 'writeable',
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
+    allowImportExportEverywhere: true,
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react', 'prettier'],
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   rules: {
-    indent: ["error", 2],
-    "no-unused-vars": [
-      "error",
+    'quote-props': ['error', 'as-needed', { keywords: true, numbers: true }],
+    indent: ['error', 2],
+    'no-unused-vars': [
+      'error',
       {
-        vars: "all",
-        args: "after-used",
+        vars: 'all',
+        args: 'after-used',
       },
     ],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
-    "react/react-in-jsx-scope": "off",
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        semi: false,
+        jsxSingleQuote: true,
+        singleQuote: true,
+        useTabs: false,
+        bracketSpacing: true,
+      },
+    ],
   },
-};
+}
