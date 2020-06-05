5508b8f3053c2350395648e73766298ced543429
GeorgeWL <GeorgeWL@users.noreply.github.com>
2020-06-05 13:53:47 +0000

    Prettified Code!

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,37 +4,37 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    React: 'writeable',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    React: "writeable",
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['react'],
+  plugins: ["react"],
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
   rules: {
-    indent: ['error', 2],
-    'no-unused-vars': [
-      'error',
+    indent: ["error", 2],
+    "no-unused-vars": [
+      "error",
       {
-        vars: 'all',
-        args: 'after-used',
+        vars: "all",
+        args: "after-used",
       },
     ],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'react/react-in-jsx-scope': 'off',
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
+    "react/react-in-jsx-scope": "off",
   },
-}
+};
