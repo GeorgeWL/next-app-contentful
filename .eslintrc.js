e55d9627c2e387c40d4c3b73afe1693f85022f80
GeorgeWL <GeorgeWL@users.noreply.github.com>
2020-06-05 13:45:41 +0000

    Prettified Code!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,40 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
-    'React': 'writeable'
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    React: "writeable",
   },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  'plugins': [
-    'react'
-  ],
-  'settings': {
-    'react': {
-      'version': 'detect'
+  plugins: ["react"],
+  settings: {
+    react: {
+      version: "detect",
     },
   },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'no-unused-vars': [
-      'error',
+  rules: {
+    indent: ["error", 2],
+    "no-unused-vars": [
+      "error",
       {
-        'vars': 'all',
-        'args': 'after-used'
-      }
+        vars: "all",
+        args: "after-used",
+      },
     ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
-    'react/react-in-jsx-scope': 'off'
-  }
-}
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
+    "react/react-in-jsx-scope": "off",
+  },
+};
